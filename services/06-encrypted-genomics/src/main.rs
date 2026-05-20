4ab18092e5949bd25bcaec14f257899ab65ba847
MrBowlin <tammo.lttn@gmail.com>
2026-05-20 22:14:23 +0200

    small changes

--- services/06-encrypted-genomics/src/main.rs
+++ services/06-encrypted-genomics/src/main.rs
@@ -1,7 +1,9 @@
 use axum::{http::StatusCode, Json};
-use aide::axum::{routing::post_with, ApiRouter};
+use aide::axum::{ApiRouter, routing::{get_with, post_with}};
+use axum::extract::DefaultBodyLimit;
 use serde::{Deserialize, Serialize};
 use schemars::JsonSchema;
+use tower_http::cors::{Any, CorsLayer};
 
 #[derive(Serialize,Deserialize,JsonSchema)]
 struct TestResponse {
@@ -16,12 +18,24 @@ pub(crate) async fn testfun() -> Result<Json<TestResponse>, (StatusCode, String)
 
 #[tokio::main]
 async fn main() {
-    let api_router = ApiRouter::new().api_route(
+    let cors = CorsLayer::new()
+        .allow_origin(Any)
+        .allow_methods(Any)
+        .allow_headers(Any);
+
+    let api_router = ApiRouter::new()
+    .api_route(
         "/", 
         post_with(testfun, |op| { 
             op.description("")
             .response::<200, Json<TestResponse>>() 
-        }),
+    }))
+    .api_route(
+        "/test",
+        get_with(testfun, |op| {
+            op.description("")
+            .response::<200, Json<TestResponse>>()
+        })
     );
     
     let app = openapi_docs::attach(
@@ -29,7 +43,10 @@ async fn main() {
         "",
         "",
         ""
-    ).merge(health::router(env!("CARGO_PKG_VERSION")));
+    )
+    .merge(health::router(env!("CARGO_PKG_VERSION")))
+    .layer(DefaultBodyLimit::max(2 * 1024 * 1024 * 1024))
+    .layer(cors);
 
     let addr = std::net::SocketAddr::from(([0, 0, 0, 0], 8080));
     let listener = tokio::net::TcpListener::bind(addr).await.unwrap();
