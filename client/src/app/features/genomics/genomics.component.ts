0a9c2cb27c8853fef1e390fb9bbc6e3fbc3bad5e
MrBowlin <tammo.lttn@gmail.com>
2026-05-31 18:35:43 +0200

    basic layout

--- client/src/app/features/genomics/genomics.component.ts
+++ client/src/app/features/genomics/genomics.component.ts
@@ -3,5 +3,6 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-genomics',
   templateUrl: './genomics.component.html',
+  styleUrl: './genomics.component.css',
 })
 export class GenomicsComponent {}
